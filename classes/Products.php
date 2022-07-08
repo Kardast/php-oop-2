4037ed40cb25c5d9a9deaede552477b08cab5d62
Kardast <dottorsf@gmail.com>
2022-07-08 16:15:53 +0200

    Products

--- classes/Products.php
+++ classes/Products.php
@@ -3,15 +3,19 @@
 class Products {
 
     private string $label = 'My Shop';
-    // private int $price;
-    // private string $bar_code;
-    // private string $brand;
+    private string $name;
+    private int $price;
+    private string $bar_code;
+    private string $brand;
+    private int $quantity;
 
-    public function __construct($price, $bar_code, $brand)
+    public function __construct($name, $price, $bar_code, $brand, $quantity)
     {
-        $this->$price = $price;
-        $this->$bar_code = $bar_code;
-        $this->$brand = $brand;
-        
+        $this->name = $name;
+        $this->price = $price;
+        $this->bar_code = $bar_code;
+        $this->brand = $brand;
+        $this->quantity = $quantity;
+    
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 include __DIR__ . '/classes/Products.php';
+include __DIR__ . '/classes/Food.php';
+include __DIR__ . '/classes/Toys.php';
+include __DIR__ . '/classes/Kennels.php';
 
-$prodotto = new Products(30, 'fogie9840', 'gattigatti');
-var_dump($prodotto);
+
+
+$prodotto_cibo = new Food('gattox', 30, 'fogie9840', 'gattigatti', 150, '1500kc', 'liquido', '3 luglio 2023');
+var_dump($prodotto_cibo);
+
+$prodotto_toy = new Toys('toydog', 300, 'fogiexggfx9840', 'canicani', 150, '300', 'plastica');
+var_dump($prodotto_toy);
+
+$prodotto_kennel = new Kennels('doghouse', 3000, 'sdsfd54dsf564', 'canicanicanicani', 15, '3000', 'legno', '500m');
+var_dump($prodotto_kennel);
